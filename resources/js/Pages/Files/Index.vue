18094217a2e1bb0711526b5f72eb1b8f6cd6a2bb
developer-tamim <tawsiftamim24@gmail.com>
2025-04-21 17:15:34 +0600

    first commit

---
+++ resources/js/Pages/Files/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>file management system</h1>
+        <h1 class="text-4xl">test</h1>
+        <h1 class="text-4xl text-red-600">test</h1>
+        <h1 class="text-4xl text-green-500">test</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
